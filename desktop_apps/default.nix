c437a434cd527bfa41b588421af4ba4e389fa663
Saketh Karumuri <skarumuri1@gmail.com>
2025-11-07 16:05:31 -0800

    cleaned up desktop_apps/default.nix

--- desktop_apps/default.nix
+++ desktop_apps/default.nix
@@ -35,21 +35,8 @@ home.file.".local/share/applications/wifi-menu.desktop".text = ''
     Exec=bash ${../tofi/wifi-menu.sh}
     Terminal=false
   '';
-  # home.file.".local/share/applications/wolfram.desktop".text = ''
-  # [Desktop Entry]
-  # Version=1.0
-  # Type=Application
-  # Name= Wolfram Notebook
-  # Exec=${pkgs.mathematica}/bin/wolframnb
-  # Terminal=false
-  # '';
 
   home.packages = with pkgs; [
-	#    (pkgs.obsidian.overrideAttrs (prevAttrs: rec {
-	#                desktopItem= (prevAttrs.desktopItem.override (d: {exec="${pkgs.obsidian}/bin/obsidian --ozone-platform-hint=auto";}));
-	# 	installPhase = builtins.replaceStrings ["${prevAttrs.desktopItem}"] ["${desktopItem}"] prevAttrs.installPhase;
-	# 	}
-	# ))
     
     (pkgs.discord.overrideAttrs (prevAttrs: rec {
                 desktopItem= (prevAttrs.desktopItem.override (d: {exec="${pkgs.discord}/bin/discord --ozone-platform-hint=auto";}));
